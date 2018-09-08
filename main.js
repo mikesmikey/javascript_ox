babd962652d42dae5c6d690e22996d55f1d655dd
Kraiwich <le_kraiwich@hotmail.com>
2018-09-08 20:11:05 +0700

    show name when winning

--- main.js
+++ main.js
@@ -1,88 +1,129 @@
-'use strict';
+//OX game using NodeJS with ECMAScript
 
-var prompt = require('prompt')
+'use strict'; //strict the variable
+
+var prompt = require('prompt') //get prompt module from npm
 var Player = require('./player.js')
-const firstPlayer =  'O'
-const getBoard = () => {
-    return {
-        1: '1', 2: '2', 3: '3',
-        4: '4', 5: '5', 6: '6',
-        7: '7', 8: '8', 9: '9'
-    }
+const firstplayer = 'X'
+const blueprintboard = () => {            //what is this thing??? quick function return?? 
+    return { 0:'0', 1:'1', 2:'2',
+             3:'3', 4:'4', 5:'5',
+             6:'6', 7:'7', 8:'8',
+ }
 }
 
-class TicTacToe {
-    constructor() {
-        this.winningLines = [
-            [1, 2, 3], [4, 5, 6], [7, 8, 9],
-            [1, 5, 9], [3, 5, 7],
-            [1, 4, 7], [2, 5, 8], [3, 6, 9]
-        ];
+const winrule = [[0, 1, 2],[3, 4, 5], [6, 7, 8],
+                 [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                 [0, 4, 8], [2, 4, 6]]
 
-        this.board = getBoard()
-
-        this.currentPlayer = firstPlayer
-
-        this.players = {
-            'O': new Player('Kraiwich'),
-            'X': new Player('Jeff')
-        }
+class OX {
+    constructor () {
+        this.currentplayer = firstplayer
+        this.board = blueprintboard()
+        this.turn = 0
+        this.xscore = 0
+        this.oscore = 0
+        this.drawscore = 0
+        this.players = {}
     }
 
-    start() {
+    gameManage() {
+        this.turn++
         this.printBoard()
         prompt.start()
-        
-        prompt.get(['location'], (err, result) => { // arrow function
-            //console.log(result.location)
-            this.setBoard(result.location)
-            if (this.checkWin()) {
-                this.printBoard()
-                console.log(`${this.currentPlayer} | ${this.players[this.currentPlayer].name} Win!`)
-                return
-            } else {
-                this.switchPlayer()
-            }
-            this.start()
-            
-        });
-        
-    }
 
-    setBoard(location) {
-        this.board[location] = this.currentPlayer
-    }
-
-    switchPlayer() {
-        if (this.currentPlayer === 'X') {
-            this.currentPlayer = 'O'
+        if (this.turn > 9) { 
+            this.drawscore++
+            this.printBoard()
+            console.log('Draw!')
+            this.printScore()
+            this.rematch()
+            return
         } else {
-            this.currentPlayer = 'X'
-        }
-    }
-
-    checkWin() {
-        let checker = 0;
-        for(let i=0; i < this.winningLines.length; i++) {
-            checker = 0;
-            for (let j = 0; j < this.winningLines[i].length; j++) {
-                if (this.board[this.winningLines[i][j]] === this.currentPlayer) {
-                  checker++;
+            console.log(`${this.currentplayer} Turn: `)
+            prompt.get(['location'], (err, result) => {
+                if (this.putToken(result.location) == false) { 
+                    console.log(`You're not putting token in the right place, Try again ..`)
+                    this.turn--
+                    this.gameManage() 
+                    return
                 }
-                if (checker === 3) {
-                    return true;
-                } 
-            }
+                
+                if (this.checkWinner()) {
+                    if (this.currentplayer === 'X') { this.xscore++ } else if (this.currentplayer === 'O') { this.oscore++ }
+                    this.printBoard()
+                    console.log(`${this.players[this.currentplayer].name} (${this.currentplayer}) Win!`)
+                    this.printScore()
+                    this.rematch()
+                    return
+                }
+                this.switchPlayer()
+                this.gameManage()
+            })
         }
-        return false;
     }
 
     printBoard() {
-        console.log(`| ${this.board[1]} | ${this.board[2]} | ${this.board[3]} |`)
-        console.log(`| ${this.board[4]} | ${this.board[5]} | ${this.board[6]} |`)
-        console.log(`| ${this.board[7]} | ${this.board[8]} | ${this.board[9]} |`)
+        console.log(`${this.board[0]} | ${this.board[1]} | ${this.board[2]}`)
+        console.log(`${this.board[3]} | ${this.board[4]} | ${this.board[5]}`)
+        console.log(`${this.board[6]} | ${this.board[7]} | ${this.board[8]}`)
+    }
+
+    putToken(location) {
+        if (this.board[location] !== 'X' && this.board[location] !== 'O' && location < 9 ) {
+            this.board[location] = this.currentplayer
+            return true
+        } else { return false }    
+    }
+
+    switchPlayer() {
+        if (this.currentplayer === 'X') { this.currentplayer = 'O'} else { this.currentplayer = 'X' }
+    }
+
+    checkWinner() {
+        for (let i = 0; i< winrule.length; i++) {
+            let check = 0
+            for (let j = 0; j< winrule[i].length; j++) {
+                
+                if (this.board[winrule[i][j]]  === this.currentplayer ) {
+                    check++ 
+                }
+            }
+            if (check == 3) {
+                return true
+            }
+        }
+        return false
+    }
+
+    printScore() {
+        console.log(`X WIN : ${this.xscore} | O WIN : ${this.oscore} | DRAW : ${this.drawscore}`)
+    }
+
+    rematch() {
+        console.log('Do you want a rematch? (Y/N)')
+        prompt.start()
+        prompt.get(['value'], (err, result) => {
+            if ( result.value === 'Y') {
+                this.board = blueprintboard()
+                this.turn = 0
+                this.gameManage()
+            } else {
+                return
+            }
+        })
+    }
+
+    setupPlayer() {
+        prompt.start()
+        prompt.get(['xname', 'oname'], (err, result) => {
+            this.players['X'] = new Player(result.xname)
+            this.players['O'] = new Player(result.oname)
+            this.gameManage()
+        })
     }
 }
 
-let game = new TicTacToe()
-game.start()
+let game = new OX()
+game.setupPlayer()
+
